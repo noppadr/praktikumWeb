ca76df11f9069b91ceb7d7222fb259a587fb3320
Nova Diana <nopadr12@gmail.com>
2024-10-09 16:32:49 +0700

    JS07_Praktikum2

---
+++ JS07_PHP_FromProcessing/empty.php
@@ -0,0 +1,15 @@
+<?php
+$myArray = array(); //Array kosong
+if (empty($myArray)) {
+    echo "Array tidak terdefinisi atau kosong.<br>";
+} else {
+    echo "Array terdifinisi dan tidak kosong.<br>";
+}
+
+if (empty($nonExistentVar)) {
+    echo "Array tidak terdefinisi atau kosong.<br>";
+} else {
+    echo "Array terdifinisi dan tidak kosong.<br>";
+}
+?>
+
