fb7f5fbe948975bb17ca4988b8a9b6b804ecbca8
Nova Diana <nopadr12@gmail.com>
2024-10-09 16:35:37 +0700

    JS07_Praktikum5

---
+++ JS07_PHP_FromProcessing/regex.php
@@ -0,0 +1,33 @@
+<?php
+
+$pattern = '/[a-z]/';
+$text = 'This is a Sample Text.';
+if (preg_match($pattern, $text)) {
+    echo "Huruf kecil ditemukan! <br>"; 
+} else {
+    echo "Tidak ada huruf kecil! <br>";
+}
+
+$pattern = '/[0-9]+/';
+$text = 'There are 123 apples.';
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0] . "<br>";
+} else {
+    echo "Tidak ada yang cocok! <br>";
+}
+
+$pattern = '/apple/';
+$replacement = 'banana';
+$text = 'I like apple pie. <br>';
+$new_text = preg_replace($pattern, $replacement, $text);
+echo $new_text; // Output: "I like banana pie."
+
+$pattern = '/go{n,m}d/'; //Cocokkan "god", "good", "gooood", dll.
+$text = 'god is good.';
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0];
+} else {
+    echo "Tidak ada yang cocok!";
+}
+
+?>
