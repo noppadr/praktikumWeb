1eb5788d2afda13d769a1f8b1c8365a5621e9226
Nova Diana <nopadr12@gmail.com>
2024-10-16 14:01:39 +0700

    mengubah isi dari file

--- JS08_PHP_FormUpload_Cookies_Session/upload.php
+++ JS08_PHP_FormUpload_Cookies_Session/upload.php
@@ -1,13 +1,18 @@
 <?php
-if (isset($_POST["submit"])) {
-    $targetdir = "uploads/"; //Direktori tujuan untuk menyimpan file
-    $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
-
-    if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
-        echo "File berhasil diunggah.";
-    }
-    else {
-        echo "Gagal mengunggah file.";
+if(isset($_POST["submit"])) {
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["myfile"]["name"]);
+    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+    $allowedTypes = array("jpg", "jpeg", "png", "gif");
+    
+    if(in_array($imageFileType, $allowedTypes) && $_FILES["myfile"]["size"] <= 500000) {
+        if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $target_file)) {
+            echo "File berhasil diunggah.";
+        } else {
+            echo "Gagal mengunggah file.";
+        }
+    } else {
+        echo "File tidak valid atau melebihi ukuran maksimum yang disetujui.";
     }
 }
 ?>
