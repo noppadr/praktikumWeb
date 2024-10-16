6fa6a7d8717661631fe183c49d180b95aecf0cb6
Nova Diana <nopadr12@gmail.com>
2024-10-16 14:19:40 +0700

    mengubah isi dari file

--- JS08_PHP_FormUpload_Cookies_Session/upload.php
+++ JS08_PHP_FormUpload_Cookies_Session/upload.php
@@ -1,25 +1,20 @@
 <?php
 if(isset($_POST["submit"])) {
-    $target_dir = "uploads/";
-    $target_file = $target_dir . basename($_FILES["myfile"]["name"]);
-    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-    $allowedTypes = array("jpg", "jpeg", "png", "gif");
+    $targetdir = "uploads/"; //Direktori tujuan untuk menyimpan file
+    $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
+    $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
     
-    if(in_array($imageFileType, $allowedTypes) && $_FILES["myfile"]["size"] <= 500000) {
-        if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $target_file)) {
-            echo "File berhasil diunggah.<br><br>";
-            
-            // Menampilkan thumbnail gambar
-            list($width, $height) = getimagesize($target_file);
-            $new_width = 200;
-            $new_height = ($height / $width) * $new_width;
-            
-            echo "<img src='$target_file' width='$new_width' height='$new_height' />";
+    $allowedExtensions = array("txt", "pdf", "doc", "docx");
+    $maxsize = 3*1024*1024;
+
+    if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"]<=$maxsize) {
+        if (move_uploaded_file($_FILES["myfile"]["tmp_name"],$targetfile)) {
+            echo "File berhasil diunggah";
         } else {
             echo "Gagal mengunggah file.";
         }
     } else {
-        echo "File tidak valid atau melebihi ukuran maksimum yang disetujui.";
+        echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan";
     }
 }
 ?>
