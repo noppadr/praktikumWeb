d7db65571ae40c56b4748c1faa21229cf784f465
Nova Diana <144871159+noppadr@users.noreply.github.com>
2024-11-13 16:51:23 +0700

    Delete JS10.2_PHP_CRUD directory

--- JS10.2_PHP_CRUD/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-// membuat token keamanan ajax request (Csrf token)
-session_start();
-if (empty($_SESSION['csrf_token'])) {
-    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
-    
-}
-?>
